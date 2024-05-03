267b838a31e9161f9acab5b377c10d5fcc94299b
Bright Mabuza <mabuza6@gmail.com>
2024-05-03 09:03:33 +0200

    Initial project.

---
+++ src/main/java/com/scraper/carsscraperapi/CarsScraperApiApplication.java
@@ -0,0 +1,13 @@
+package com.scraper.carsscraperapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsScraperApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsScraperApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scraper/carsscraperapi/CarsScraperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scraper.carsscraperapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsScraperApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
